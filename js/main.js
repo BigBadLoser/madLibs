8d0547d8d4ee5ea37a88263749100657fa4aa33f
Trey Gaulin <loserizer@gmail.com>
2017-04-29 08:41:11 +0000

    random gen works, need to add more words to the lists.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,21 @@ var madLibOutput = document.getElementById("madLibOutput");
 var inputOptions = document.getElementById("inputOptions");
 var ou = document.querySelector("#madLibOutput");
 var currentLib;
-// '<input type="text" class="form-control" placeholder="Verb">'
+var verbs = ["run", "walk", "kill", "throw", "eat", "ski", "fall"];
+var nouns = ["cat", "dog", "horse", "chicken", "potato", "lead-based paint"];
+var adjectives = ["juicy", "watery"];
+var bodyParts = ["arm", "leg", "hand", "foot", "abdomen"];
+var locations = ["Paris", "Georgia"]; // all 50 states
+var celebrities = ["Tom Cruise", "Matt Damon", "Leonardo DiCaprio"];
+var livingThings = ["Dog", "Tree", "Cucumber"];
+var colors = ["blue", "red", "white"];
+var householdItems = ["lamp", "television"];
+var exclamations = ["Eureka!", "Daddy!", "Wow!"];
+var monsters = ["werewolf", "vampire"];
+var clothing = ["shirt", "pants", "shoes"];
+var companies = ["Taco Bell", "Walmart", "Chick-fil-a"];
+var names = ["Todd", "Jimmy", "Johnny"];
 
-//Instead of 0, I should really just check if they have a value at all during my loops, but oh well.
 var shakespeareSonnet = {
     verbs: 5,
     nouns: 10,
@@ -14,86 +26,49 @@ var shakespeareSonnet = {
     celebrities: 1,
     livingThings: 2,
     colors: 1,
-    householdItems: 0,
-    exclamations: 0,
-    monsters: 0,
-    clothing: 0,
     filePath: "libs/shakespeareSonnet.txt",
     title: "SHAKESPEARE SONNET 18",
 };
-//1 household item, 4 verb, 1 adj., 2 noun, 1 article of clothing, 3 body part, 1 exclamation, 
 var coolestGirl = {
     verbs: 4,
     nouns: 2,
     adjectives: 1,
     bodyParts: 3,
     locations: 0,
-    celebrities: 0,
-    livingThings: 0,
-    colors: 0,
     householdItems: 1,
     exclamations: 1,
-    monsters: 0,
     clothing: 1,
-    names: 0,
-    companies: 0,
     filePath: "libs/coolestGirl.txt",
     title: "How To Date the Coolest Girl in School",
 };
 var afraid = {
-    verbs: 4,
+    verbs: 5,
     nouns: 6,
-    adjectives: 0,
     bodyParts: 4,
-    locations: 0,
-    celebrities: 0,
-    livingThings: 0,
-    colors: 0,
-    householdItems: 0,
     exclamations: 1,
     monsters: 1,
-    clothing: 0,
-    names: 0,
-    companies: 0,
     filePath: "libs/afraid.txt",
     title: "Afraid of the Dark",
 };
 
-//1 body part, 4 noun, 2 animal, 4 adj., 1 vegetable, 1 name
 var vegan = {
-    verbs: 0,
     nouns: 4,
     adjectives: 4,
     bodyParts: 1,
-    locations: 0,
     celebrities: 0,
     livingThings: 3,
-    colors: 0,
-    householdItems: 0,
-    exclamations: 0,
     monsters: 1,
-    clothing: 0,
     names: 1,
-    companies: 0,
     filePath: "libs/vegan.txt",
     title: "Vegan Stories",
 };
 
-//7 nouns, 2 locations, 2 adj., 1 form of transportation, 2 company, 1 name, 1 job title, 1 verb-ed
 
 var starWars = {
     verbs: 1,
     nouns: 7,
     adjectives: 2,
-    bodyParts: 0,
     locations: 2,
-    celebrities: 0,
-    livingThings: 0,
-    colors: 0,
-    householdItems: 0,
-    exclamations: 0,
-    monsters: 0,
-    clothing: 0,
     names: 1,
     companies: 2,
     filePath: "libs/starWars.txt", 
@@ -162,7 +137,54 @@ function onButtonClick(lib) {
     currentLib = lib;
     setInput(lib);
 }
-
+function generateRandom() {
+    var lib = currentLib;
+    var txt = readStringFromFileAtPath (lib.filePath);
+    for(var i = 1; i <= lib.nouns; i++){
+        txt = txt.replace("$noun" + i, nouns[Math.floor(Math.random() * nouns.length)]);
+    }
+    for(var i = 1; i <= lib.adjectives; i++){
+        txt = txt.replace("$adj" + i, adjectives[Math.floor(Math.random() * adjectives.length)]);
+    }
+    for(var i = 1; i <= lib.verbs; i++){
+        txt = txt.replace("$verb" + i, verbs[Math.floor(Math.random() * verbs.length)]);
+    }
+    for(var i = 1; i <= lib.colors ; i++){
+        txt = txt.replace("$color" + i, colors[Math.floor(Math.random() * colors.length)]);
+    }
+    for(var i = 1; i <= lib.bodyParts; i++){
+        txt = txt.replace("$bodyPart" + i, bodyParts[Math.floor(Math.random() * bodyParts.length)]);
+    }
+    for(var i = 1; i <= lib.celebrities; i++){
+        txt = txt.replace("$celebrity" + i, celebrities[Math.floor(Math.random() * celebrities.length)]);
+    }
+    for(var i = 1; i <= lib.livingThings ; i++){
+        txt = txt.replace("$livingThing" + i, livingThings[Math.floor(Math.random() * livingThings.length)]);
+    }
+    for(var i = 1; i <= lib.locations ; i++){
+        txt = txt.replace("$location" + i, locations[Math.floor(Math.random() * locations.length)]);
+    }
+    for(var i = 1; i <= lib.householdItems ; i++){
+        txt = txt.replace("$householdItem" + i, householdItems[Math.floor(Math.random() * householdItems.length)]);
+    }
+    for(var i = 1; i <= lib.exclamations ; i++){
+        txt = txt.replace("$exclamation" + i, exclamations[Math.floor(Math.random() * exclamations.length)]);
+    }
+    for(var i = 1; i <= lib.monsters ; i++){
+        txt = txt.replace("$monster" + i, monsters[Math.floor(Math.random() * monsters.length)]);
+    }
+    for(var i = 1; i <= lib.clothing ; i++){
+        txt = txt.replace("$clothing" + i, clothing[Math.floor(Math.random() * clothing.length)]);
+    }
+    for(var i = 1; i <= lib.names ; i++){
+        txt = txt.replace("$name" + i, names[Math.floor(Math.random() * names.length)]);
+    }
+    for(var i = 1; i <= lib.companies; i++){
+        txt = txt.replace("$company" + i, companies[Math.floor(Math.random() * companies.length)]);
+    }    
+    madLibOutput.innerHTML = txt;
+    ou.style.textAlign = "left";
+}
 //document.getElementById("searchTxt").value;
 function readStringFromFileAtPath(pathOfFileToReadFrom)
     {
