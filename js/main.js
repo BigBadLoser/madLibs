bcb84dc9fe529633511c8ef3b7837b72dd983713
Trey Gaulin <loserizer@gmail.com>
2017-04-29 07:15:50 +0000

    added 2 new libs, some new options for words, and some bug fixes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var madLibOutput = document.getElementById("madLibOutput");
 var inputOptions = document.getElementById("inputOptions");
+var ou = document.querySelector("#madLibOutput");
 var currentLib;
 // '<input type="text" class="form-control" placeholder="Verb">'
 var shakespeareSonnet = {
@@ -11,8 +12,46 @@ var shakespeareSonnet = {
     celebrities: 1,
     livingThings: 2,
     colors: 1,
-    filePath: "https://preview.c9users.io/bigbadloser/madlibs/libs/shakespeareSonnet.txt", //VERY IMPORTANT, CHANGE THIS BEFORE COMMITING TO GITHUB!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    householdItems: 0,
+    exclamations: 0,
+    monsters: 0,
+    clothing: 0,
+    filePath: "https://bigbadloser.github.io/madlibs/libs/shakespeareSonnet.txt", //"https://preview.c9users.io/bigbadloser/madlibs/libs/shakespeareSonnet.txt"
     title: "SHAKESPEARE SONNET 18",
+};
+//1 household item, 4 verb, 1 adj., 2 noun, 1 article of clothing, 3 body part, 1 exclamation, 
+var coolestGirl = {
+    verbs: 4,
+    nouns: 2,
+    adjectives: 1,
+    bodyParts: 3,
+    locations: 0,
+    celebrities: 0,
+    livingThings: 0,
+    colors: 0,
+    householdItems: 1,
+    exclamations: 1,
+    monsters: 0,
+    clothing: 1,
+    filePath: "https://bigbadloser.github.io/madlibs/libs/coolestGirl.txt", //"https://preview.c9users.io/bigbadloser/madlibs/libs/coolestGirl.txt"
+    title: "How To Date the Coolest Girl in School",
+};
+//6 noun/s, 4 verb-ing, 4 body part/s, 1 monster, 1 exclamation 
+var afraid = {
+    verbs: 4,
+    nouns: 6,
+    adjectives: 0,
+    bodyParts: 4,
+    locations: 0,
+    celebrities: 0,
+    livingThings: 0,
+    colors: 0,
+    householdItems: 0,
+    exclamations: 1,
+    monsters: 1,
+    clothing: 0,
+    filePath: "https://bigbadloser.github.io/madlibs/libs/afraid.txt", //"https://preview.c9users.io/bigbadloser/madlibs/libs/afraid.txt"
+    title: "Afraid of the Dark",
 };
 function createInputArray(lib){
     var inputOptionsA = [];
@@ -37,9 +76,24 @@ function createInputArray(lib){
     for (var i = 1; i <= lib.livingThings; i++){
         inputOptionsA.push('<input type="text" class="form-control" placeholder="Living Thing' + ' ' + i + '"' + 'id="livingThing' + i + '">');
     }
-    for (var i = 1; i <= lib.livingThings; i++){
+    for (var i = 1; i <= lib.colors; i++){
         inputOptionsA.push('<input type="text" class="form-control" placeholder="Color' + ' ' + i + '"' + 'id="color' + i + '">');
     }
+    for (var i = 1; i <= lib.householdItems; i++){
+        inputOptionsA.push('<input type="text" class="form-control" placeholder="Household item' + ' ' + i + '"' + 'id="householdItem' + i + '">');
+    }
+    for (var i = 1; i <= lib.exclamations; i++){
+        inputOptionsA.push('<input type="text" class="form-control" placeholder="Exclamation' + ' ' + i + '"' + 'id="exclamation' + i + '">');
+    }
+    for (var i = 1; i <= lib.monsters; i++){
+        inputOptionsA.push('<input type="text" class="form-control" placeholder="Monster ' + ' ' + i + '"' + 'id="monster' + i + '">');
+    }
+    for (var i = 1; i <= lib.clothing; i++){
+        inputOptionsA.push('<input type="text" class="form-control" placeholder="Clothing Item ' + ' ' + i + '"' + 'id="clothing' + i + '">');
+    }
+    
+    madLibOutput.innerHTML = '<button type="button" class="btn btn-default" onclick="outputLib()" id="outputButton">Output Lib</button>';
+    ou.style.textAlign = "center";
     return inputOptionsA;
 }
 
@@ -49,6 +103,7 @@ function setInput(lib){
 function outputLib(){
     var output = getOutput(currentLib);
     madLibOutput.innerHTML = output;
+    ou.style.textAlign = "left";
     
 }
 function onButtonClick(lib) {
@@ -69,7 +124,7 @@ function readStringFromFileAtPath(pathOfFileToReadFrom)
     
 function getOutput(lib){
     var txt = readStringFromFileAtPath (lib.filePath);
-    for(var i = 1; i <= 10; i++){
+    for(var i = 1; i <= lib.nouns; i++){
         txt = txt.replace("$noun" + i, document.getElementById("noun" + i).value);
     }
     for(var i = 1; i <= lib.adjectives; i++){
@@ -92,6 +147,18 @@ function getOutput(lib){
     }
     for(var i = 1; i <= lib.locations ; i++){
         txt = txt.replace("$location" + i, document.getElementById("location" + i).value);
+    }
+    for(var i = 1; i <= lib.householdItems ; i++){
+        txt = txt.replace("$householdItem" + i, document.getElementById("householdItem" + i).value);
+    }
+    for(var i = 1; i <= lib.exclamations ; i++){
+        txt = txt.replace("$exclamation" + i, document.getElementById("exclamation" + i).value);
+    }
+    for(var i = 1; i <= lib.monsters ; i++){
+        txt = txt.replace("$monster" + i, document.getElementById("monster" + i).value);
+    }
+    for(var i = 1; i <= lib.clothing ; i++){
+        txt = txt.replace("$clothing" + i, document.getElementById("clothing" + i).value);
     }
     return txt;
 }    
